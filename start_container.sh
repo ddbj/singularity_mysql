58b3ca5e7a8a3db2d96d059d0e78aae5e797978e
okuda <yookuda@nig.ac.jp>
2021-03-15 18:58:38 +0900

    インスタンス起動時にmysql_dataをホームディレクトリに設定

--- start_container.sh
+++ start_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONTAINER_HOME="/home/user/singularity_mysql"
+CONTAINER_HOME=$(cd $(dirname $0); pwd)
 IMAGE="ubuntu-18.04-mysql-5.6.46.simg"
 INSTANCE="mysql"
 
@@ -11,6 +11,7 @@ fi
 singularity instance.start \
 -B ${CONTAINER_HOME}/mysql_data:/usr/local/mysql/data \
 -B ${CONTAINER_HOME}/my_mysql.cnf:/etc/my.cnf \
+-H ${CONTAINER_HOME}/mysql_data \
 ${CONTAINER_HOME}/${IMAGE} \
 ${INSTANCE}
 
