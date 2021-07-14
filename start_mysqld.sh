deb1bbb1d16d445c681ca7043785598f8b39b695
okuda <yookuda@nig.ac.jp>
2021-07-14 16:26:30 +0900

    mysqld起動時にMYSQL_HOME環境変数を設定して~/.my.cnfを参照しないようにした。

--- start_mysqld.sh
+++ start_mysqld.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-/usr/local/mysql/bin/mysqld > /usr/local/mysql/data/mysql.log 2>&1 &
+# 本スクリプトの場所をMYSQL_HOMEに設定して~/.my.cnfを参照しないようにする。
+export MYSQL_HOME=$(cd $(dirname $0); pwd)
+
+/usr/local/mysql/bin/mysqld --defaults-file=/usr/local/mysql/my_mysql.cnf > /usr/local/mysql/data/mysql.log 2>&1 &
 
